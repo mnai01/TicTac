ee04c9a66b0058fb613ee436491384a3787c54b4
mnai01 <imatlak03@gmail.com>
2019-05-15 16:47:06 -0400

    test

--- tictac.py
+++ tictac.py
@@ -1,1 +1,28 @@
-# test
+board = [' ' for x in range(10)]
+
+
+def printBoard(board):
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('------------')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('------------')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+
+
+def getMove():
+    for x in range(10):
+        printBoard(board)
+        playerMove = int(input("Select your move: "))
+
+        if playerMove < 1 or playerMove > 9:
+            print("move out of range: ")
+            continue
+
+        if board[playerMove] != ' ':
+            print("space is already taken")
+            continue
+        else:
+            board[playerMove] = 'x'
+
+
+getMove()
