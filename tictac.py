c29173f6c5079edb17ae3e6caddda33d0a71c06a
mnai01 <imatlak03@gmail.com>
2019-05-15 17:00:06 -0400

    test merge

--- tictac.py
+++ tictac.py
@@ -1,39 +1,11 @@
-<<<<<<< HEAD
-
 board = [' ' for x in range(10)]
+
 
 def printBoard(board):
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('----------')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('----------')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-
-
-def main():
-    for x in range(10):
-        printBoard(board)
-        userMove = int(input("Enter move: "))
-        if userMove < 1 or userMove > 9:
-            print("not a valid move. Try again")
-            continue
-        if board[userMove] != ' ':
-            print('That space is already taken. Try again')
-            continue
-        else:
-            board[userMove] = 'x'
-       
-
-main()
-=======
-board = [' ' for x in range(10)]
-
-
-def printBoard(board):
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('------------')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('------------')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
 
 
@@ -54,4 +26,3 @@ def getMove():
 
 
 getMove()
->>>>>>> test
